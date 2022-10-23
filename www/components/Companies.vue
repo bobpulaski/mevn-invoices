de76ded7356fea92b5bcdc7022de7fddc56accb7
Bob Pulaski Home <mynameisempty@yandedex.ru>
2022-10-23 15:14:11 +0300

    '23-10-22-home'

--- www/components/Companies.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <h1>Companies component</h1>
-</template>
--- www/src/main.js
+++ www/src/main.js
@@ -1,6 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import companies from 'components/Companies.vue';
 
 createApp(App).mount('#app');
-App.mounted('companies', companies);
