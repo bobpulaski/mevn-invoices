7e88d7ad78e967587fb93dd723fb2eaf657d6d09
Bob Pulaski Home <mynameisempty@yandedex.ru>
2022-11-22 21:07:10 +0300

    22-11-22-home

--- www/components/content/companies/kso-delete-modal-confirm.vue
+++ www/components/content/companies/kso-delete-modal-confirm.vue
@@ -5,16 +5,15 @@
       <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
           <div class="modal-header">
-            <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+            <h1 class="modal-title fs-5" id="exampleModalLabel">Удалить?</h1>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
-            {{ companyId }}
-            {{ companyName }}
+            <p>Вы уверены, что хотите удалить {{ companyName }}? Данное действие отменить не возможно.</p>
           </div>
           <div class="modal-footer">
-            <button id="closeButton" type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary" v-on:click="deleteCompany(companyId)">Save changes</button>
+            <button id="closeButton" type="button" class="btn btn-secondary" data-bs-dismiss="modal">Отмена</button>
+            <button type="button" class="btn btn-primary" v-on:click="deleteCompany(companyId)"> .тра та та</button>
           </div>
         </div>
       </div>
@@ -46,5 +45,4 @@
 </script>
 
 <style scoped>
-
 </style>
