a6985a50a30139e9e2369c59d6081cf74bccd10e
Bob Pulaski Home <mynameisempty@yandedex.ru>
2022-11-23 23:03:42 +0300

    23-11-2022-home

--- routes/api.js
+++ routes/api.js
@@ -1,56 +1,65 @@
 const express = require("express");
-const {Router, response} = require("express");
+const { Router, response } = require("express");
 const router = Router();
 const mysql = require("mysql2");
 
 router.use(express.json());
 
 const connection = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    database: "mevn-invoice",
+  host: "localhost",
+  user: "root",
+  database: "mevn-invoice",
 });
 
 router.get("/company", async (req, res) => {
-    try {
-        await connection.execute(
-            "SELECT * FROM `companies`",
-            function (err, results) {
-                if (err) console.log(err);
-                return res.status(200).json(results);
-            }
-        );
-    } catch (e) {
-        res.status(500).json({message: "Error on Company route"});
-    }
+  try {
+    await connection.execute(
+      "SELECT * FROM `companies`",
+      function (err, results) {
+        if (err) console.log(err);
+        return res.status(200).json(results);
+      }
+    );
+  } catch (e) {
+    res.status(500).json({ message: "Error on Company route" });
+  }
+});
+
+router.get("/company/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    let sql = "SELECT * FROM companies WHERE id = ?";
+    await connection.execute(sql, [id], function (err, results) {
+      if (err) console.log(err);
+      res.status(200).json(results);
+    });
+  } catch (e) {}
 });
 
 router.post("/company", async (req, res) => {
-    try {
-        const {name, inn} = req.body;
-        console.log(req.body);
-        let sql = "INSERT INTO companies (name, inn) VALUES (?, ?)";
+  try {
+    const { name, inn } = req.body;
+    console.log(req.body);
+    let sql = "INSERT INTO companies (name, inn) VALUES (?, ?)";
 
-        await connection.execute(sql, [name, inn], function (err, results) {
-            if (err) console.log(err);
-            return res.status(200).json(results);
-        });
-    } catch (e) {
-        res.status(500).json({message: "Error on Company/POST route"});
-    }
+    await connection.execute(sql, [name, inn], function (err, results) {
+      if (err) console.log(err);
+      return res.status(200).json(results);
+    });
+  } catch (e) {
+    res.status(500).json({ message: "Error on Company/POST route" });
+  }
 });
 
 router.delete("/company/:id", async (req, res) => {
-    try {
-        const {id} = req.params;
-        let sql = "DELETE FROM companies WHERE id = ?";
-        await connection.execute(sql, [id], function (err, results, next) {
-          if (err) console.log(err);
-          res.status(200).json(results);
-        });
-    } catch (e) {
-
-    }
+  try {
+    const { id } = req.params;
+    let sql = "DELETE FROM companies WHERE id = ?";
+    await connection.execute(sql, [id], function (err, results, next) {
+      if (err) console.log(err);
+      res.status(200).json(results);
+    });
+  } catch (e) {}
 });
 
 module.exports = router;
--- www/components/content/companies/kso-company-add-edit.vue
+++ www/components/content/companies/kso-company-add-edit.vue
@@ -1,32 +1,42 @@
 <template>
   <h1>Company Add Edit Page</h1>
-  <h2>
-    <div>{{ $route.query.type }}</div>
-  </h2>
-  <h2>
-    <div>{{ $route.params.id }}</div>
-    <div>{{ decryptedId }}</div>
-  </h2>
+  <h2>{{ companies.id }}</h2>
+  <input type="text" class="form-control" v-model="companies.name">
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "ksoCompanyAddEdit",
 
   data() {
     return {
-      type: [],
+      companies: [{}],
     };
   },
 
-  mounted() {
-    //console.log(this.$route.query.type);
+  methods: {
+    async getCompanyById() {
+      const decryptedId = this.$CryptoJS.AES.decrypt(
+        this.$route.params.id,
+        "jaga"
+      ).toString(this.$CryptoJS.enc.Utf8);
 
-    const decryptedId = this.$CryptoJS.AES.decrypt(
-      this.$route.params.id,
-      "jaga"
-    ).toString(this.$CryptoJS.enc.Utf8);
-    console.log(decryptedId);
+      await axios
+        .get("/api/company/" + decryptedId)
+        .then((response) => {
+          this.companies = response.data[0];
+          console.log(this.companies);
+        })
+        .catch((e) => {
+          this.errors.push(e);
+        });
+    },
+  },
+
+  mounted() {
+    this.getCompanyById();
   },
 };
 </script>
